3c9f2e9526cfa3e0f0544af29bbc6ae85ccdce5b
Joshua <joshuaamaral05@gmail.com>
2022-01-04 01:12:47 -0300

    Sistema de Cadastro-Eventos feito, seguindo requesitos

---
+++ sistemaCadastro.js
@@ -0,0 +1,31 @@
+let dataAtual = 04/01/2022
+
+console.log ("Informar data do evento")
+
+let dataEvento = 04/02/2022
+
+if (dataEvento > dataAtual) {
+    console.log ("Data do evento válida")
+} else {
+ console.log ("Data inválida")   
+}
+
+let idadeUsuario = 19
+
+console.log ("Informe sua idade para prosseguir o cadastro")
+
+if (idadeUsuario >= 18) {
+    console.log ("Cadastro de usuário permitido")
+} else {
+    console.log ("Cadastro de usuário não permitido")
+}
+
+//Listar  participantes e palestrantes por evento ( descobrir forma de mostrar)
+
+let quantidadeParticipantes = 99
+
+if (quantidadeParticipantes < 100) {
+    console.log ("Cadastro permitido")
+} else {
+    console.log ("ERRO: Cadastro excedeu o limite")
+}
